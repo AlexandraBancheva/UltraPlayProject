cc15e4add48f4a42d9a662180d7720048a4e6374
AlexandraBancheva <sandriche90@gmail.com>
2023-01-16 16:18:17 +0200

    Added Controller

---
+++ UltraPlayProject/Controllers/UltraPlayController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UltraPlayProject.Controllers
+{
+    [ApiController]
+    [Route("api/UltraPlay")]
+    public class UltraPlayController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult AllMatchesStartingBy24Hours()
+        {
+            return Ok();
+        }
+
+        [HttpGet("id")]
+        //i.match name
+        //ii.match start date
+        //iii.match's active markets with all their active odds
+        //iv.match's inactive markets with their odds
+        // Polymorphysm
+        public IActionResult MatchByGivenUniqueIdentifier(string id) 
+        {
+            return Ok();
+        }
+    }
+}
